626c9c3a9dba965c2b11380420650bba761f8fbc
leesj <leesj@Jun-ThinkPad-E580>
2021-12-04 15:25:52 +0900

    컴파일 버전 1.8로 변경 및 AWS 주소 추가

--- shop/src/commons/DBUtil.java
+++ shop/src/commons/DBUtil.java
@@ -8,6 +8,7 @@ public class DBUtil {
 	public Connection getConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("org.mariadb.jdbc.Driver");
 		Connection conn = DriverManager.getConnection("jdbc:mariadb://127.0.01:3306/shop", "root", "java1004");
+		//Connection conn = DriverManager.getConnection("jdbc:mariadb://15.165.194.216/shop", "root", "java1004");
 		return conn;
 	}
 }
